177af217de9fcd684058d77bb5d4849074950bb2
Christian Christiansen <christcl01@hotmail.com>
2019-09-26 00:03:24 +0200

    minor changes

--- projecteuler/1.py
+++ projecteuler/1.py
@@ -1,7 +1,7 @@
 i = 0
 sum = 0
 while i < 1000:
-	if i%5 == 0 or i%7 == 0 or i%11 == 0:
-		sum = sum + i
-	i = i+1
+    if i%5 == 0 or i%7 == 0 or i%11 == 0: 
+        sum = sum + i
+    i = i+1
 print sum
--- projecteuler/1a.py
+++ projecteuler/1a.py
@@ -6,21 +6,11 @@ def triangular(a,b):
     return a*n*(n+1)/2
 
 def addon(i, vtriangular, nums, limit):
-    print(nums)
     return((-1)**(i%2)*vtriangular(list(np.prod(x) for x in list(itertools.combinations(nums,i+1))),limit))
-
-def addon1(i, vtriangular, num, limit):
-    print(num)
-    return i
-    #return((-1)**(i%2)*vtriangular(nums)))
 
 limit = 1000
 nums = [5,7,11]
 
 vtriangular = np.vectorize(triangular)
 
-vaddon = np.vectorize(addon1)
-
-print(sum(list(sum(vaddon(range(len(nums)),vtriangular, nums, limit)))))
-
-#print(sum(list(sum(addon(i,vtriangular,nums,limit)) for i in range(len(nums)))))
+print(sum(list(sum(addon(i,vtriangular,nums,limit)) for i in range(len(nums)))))
