f940b1f98269433b896d841d7e95815bf94de342
Christian Christiansen <christcl01@hotmail.com>
2019-09-27 18:28:48 +0200

    Remove Project Euler to not spoil the code for anybody

--- projecteuler/3.py
+++
@@ -1,9 +0,0 @@
-x = 600851475147
-
-def largestprimefactor(x):
-    for i in range(2,int(x**0.5)+1,1):
-        if x%i == 0:
-            return largestprimefactor(x/i)
-    return x
-
-print(largestprimefactor(x))
